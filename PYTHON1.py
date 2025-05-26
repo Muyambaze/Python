f00712f85cf5082e837078c545ba7a78d0e39ca5
Muyambaze <samuel.muyambaze@ashesi.edu.gh>
2025-05-26 17:08:49 +0000

    Create PYTHON1.py

---
+++ PYTHON1.py
@@ -0,0 +1,10 @@
+print("Hello world")
+name=input("What is your name?:")
+mood=input("Rate your mood:")
+num_mood=int(mood)
+total_mood=num_mood+8+9
+avarage=total_mood/3
+print(f"{name}")
+print(f"{mood}")
+print(f"Average={avarage}")
+print("It has been an amazing day")
